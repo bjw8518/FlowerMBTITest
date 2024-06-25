cdd02e7104cb331646a78043f626341245b2f0a2
bjw <bjw8518@gmail.com>
2024-06-25 18:06:26 +0900

    240625

---
+++ src/main/java/com/bjw/FlowerMBTITest/FlowerMbtiTestApplication.java
@@ -0,0 +1,13 @@
+package com.bjw.FlowerMBTITest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerMbtiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerMbtiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bjw/FlowerMBTITest/FlowerMbtiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjw.FlowerMBTITest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerMbtiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
